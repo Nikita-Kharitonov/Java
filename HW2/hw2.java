fc8b3e0843e94c4ee13541f13b3e320336d07fa8
Nikita_Kharitonov <118862999+Nikita-Kharitonov@users.noreply.github.com>
2023-04-08 15:49:26 +0700

    1

---
+++ HW2/hw2.java
@@ -0,0 +1,36 @@
+package HW2;
+
+// Дана строка (получение через обычный текстовый файл!!!)
+
+// "фамилия":"Иванов","оценка":"5","предмет":"Математика"
+// "фамилия":"Петрова","оценка":"4","предмет":"Информатика"
+
+// Написать метод(ы), который распарсит строку и, используя StringBuilder, создаст строки вида:
+// Студент [фамилия] получил [оценка] по предмету [предмет].
+
+// Пример вывода:
+// Студент Иванов получил 5 по предмету Математика.
+// Студент Петрова получил 4 по предмету Информатика.
+// Студент Краснов получил 5 по предмету Физика.
+
+import java.io.File;
+import java.util.Scanner;
+
+
+public class hw2 {
+
+     
+    public static void main(String[] args) throws Exception {
+
+        File file = new File("task_2.json");
+        Scanner scanner = new Scanner(file); 
+        String line = scanner.nextLine();
+        String line2 = line.replace("{", "").replace("}","").replace(":",",");
+        String [] lineData = line2.split(",");
+        String family = lineData[1];
+        String mark = lineData[3];
+        String subject = lineData[5];
+        scanner.close();        
+        System.out.printf("Студент %s получил %s по предмету %s  " , family, mark, subject);
+        }
+}
