4d8291f26b6a7c7ced775e720f8e1bd3d3791db2
Nikita_Kharitonov <118862999+Nikita-Kharitonov@users.noreply.github.com>
2023-04-14 07:12:22 +0700

    1

---
+++ hw2.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class hw2 {
+    public static void main(String[] args) {
+        String[] nameWorkers = {"Иван", "Вася", "Петя", "Сергей", "Григорий", "Григорий",
+                "Петя", "Алла", "Антон", "Иван", "Ольга", "Иван", "Сергей", "Григорий", "Иван"};
+
+        HashMap<String, Integer> workers = new HashMap<>();
+        for (String i : nameWorkers) {
+            int newValue = workers.getOrDefault(i, 0) + 1;
+            workers.put(i, newValue);
+        }
+        System.out.println(workers);
+
+        
+        int max = 1;
+        for (int value : workers.values()) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        for (int i = max; i > 0; i--) {
+            for (Map.Entry<String, Integer> entry : workers.entrySet()) {
+                String key = entry.getKey();
+                if (workers.get(key) == i) {                    
+                    System.out.format("%s %d \n", key, workers.get(key));
+
+                }
+            }
+        }
+
+
+    }
+
+
+}
